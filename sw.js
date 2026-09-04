823476c77122fd3aa05dc00bfc968e9f26557693
VBento9 <163012722+VBento9@users.noreply.github.com>
2026-09-04 13:48:13 +0100

    V10.0.9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='donart-v10-0-8-online-orders';
+const CACHE='donart-v10-0-9-online-detail';
 const CORE=['/','/manifest.webmanifest','/icon-192.png','/icon-512.png','/logo-donart.png','/logo-donart-ui.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim()})())});
