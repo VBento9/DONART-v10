3973b5bad37eae7521a46e96fe2ef09c1bd24cfc
VBento9 <163012722+VBento9@users.noreply.github.com>
2026-09-08 08:51:54 +0100

    V12.6.13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='donart-v12-6-12';
+const CACHE_NAME='donart-v12-6-13';
 const STATIC_ASSETS=['./icon-192.png','./icon-512.png','./logo-donart-ui.png','./logo-donart.png'];
 
 self.addEventListener('install',event=>{
