dbf7be303bab68307c7e7b159333ca832e778352
VBento9 <163012722+VBento9@users.noreply.github.com>
2026-09-10 21:02:44 +0100

    12.6.27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='donart-v12-6-25-shell';
+const CACHE='donart-v12-6-27-shell';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-donart-ui.png','./logo-donart.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL).catch(()=>{})).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
